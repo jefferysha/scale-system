0c8a0572ff6f08ad9549af861e52525889b6eccd
shajunyi <shajunyi@jiayinfintech.cn>
2026-05-03 23:50:45 +0800

    feat(api): weighing_records 模型 + JSONB GIN 索引

--- apps/api/src/scale_api/models/__init__.py
+++ apps/api/src/scale_api/models/__init__.py
@@ -4,6 +4,7 @@ from scale_api.models.base import Base
 from scale_api.models.cup import Cup
 from scale_api.models.cup_calibration import CupCalibration
 from scale_api.models.project import Project
+from scale_api.models.record import WeighingRecord
 from scale_api.models.refresh_token import RefreshToken
 from scale_api.models.scale import Scale
 from scale_api.models.user import User
@@ -19,4 +20,5 @@ __all__ = [
     "Scale",
     "User",
     "Vertical",
+    "WeighingRecord",
 ]
